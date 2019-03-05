446ef296caa5ac5ea72ac13f9b12e76835c8b711
Mona Lisa <andrewstockus@gamil.com>
2019-03-05 17:35:57 -0500

    Final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
 		name: "Kevin",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		photo: "https://www.thehindu.com/migration_catalog/article10268775.ece/alternates/FREE_660/bean",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Jose",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		photo: "https://www.thehindu.com/migration_catalog/article10268775.ece/alternates/FREE_660/bean",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Ralph",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		photo: "https://www.thehindu.com/migration_catalog/article10268775.ece/alternates/FREE_660/bean",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "Jim",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		photo: "https://www.thehindu.com/migration_catalog/article10268775.ece/alternates/FREE_660/bean",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
 		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://www.thehindu.com/migration_catalog/article10268775.ece/alternates/FREE_660/bean",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
